66fe2597df082ae752d0de73ae6c6d96963dab8e
egor-denysenko <75579636+egor-denysenko@users.noreply.github.com>
2024-04-25 15:34:35 +0200

    feature: reply keyboard markup implementation (#7)
    * feat(keyboard): implemented rendering of a reply keyboard
    
    * chore(readme): adjusted documentation links to images
    
    * chore(reply_keyboard): fixed inline keyboard documentation link and added reply keyboard readme

---
+++ examples/reply_keyboard.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/go-telegram/ui/keyboard/reply"
+)
+
+var demoReplyKeyboard *reply.ReplyKeyboard
+
+func initReplyKeyboard(b *bot.Bot) {
+	demoReplyKeyboard = reply.New(
+		b,
+		reply.WithPrefix("reply_keyboard"),
+		reply.IsSelective(),
+		reply.IsOneTimeKeyboard(),
+	).
+		Button("Button", b, bot.MatchTypeExact, onReplyKeyboardSelect).
+		Row().
+		Button("Cancel", b, bot.MatchTypeExact, onReplyKeyboardSelect)
+}
+
+func handlerReplyKeyboard(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      update.Message.Chat.ID,
+		Text:        "Select example command from reply keyboard:",
+		ReplyMarkup: demoReplyKeyboard,
+	})
+}
+
+func onReplyKeyboardSelect(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   "You selected: " + string(update.Message.Text),
+	})
+}
---
+++ keyboard/reply/add.go
@@ -0,0 +1,23 @@
+package reply
+
+import (
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func (kb *ReplyKeyboard) Row() *ReplyKeyboard {
+	if len(kb.markup[len(kb.markup)-1]) > 0 {
+		kb.markup = append(kb.markup, []models.KeyboardButton{})
+	}
+	return kb
+}
+
+func (kb *ReplyKeyboard) Button(text string, b *bot.Bot, matchType bot.MatchType, handler bot.HandlerFunc) *ReplyKeyboard {
+	kb.markup[len(kb.markup)-1] = append(kb.markup[len(kb.markup)-1], models.KeyboardButton{
+		Text: text,
+	})
+
+	b.RegisterHandler(bot.HandlerTypeMessageText, text, matchType, handler)
+
+	return kb
+}
